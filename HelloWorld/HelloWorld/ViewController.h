7cc55761deea1edbc951f366fb6f40ba37fa4df0
iToplogic <kisswolf33@naver.com>
2015-07-22 10:24:42 +0900

    HelloWorld
    add HelloWorld

---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by JEONSANGIL on 2015. 7. 22..
+//  Copyright (c) 2015년 Toplogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *displayLabel;
+@property (weak, nonatomic) IBOutlet UIButton *goBtn;
+
+@end
+
